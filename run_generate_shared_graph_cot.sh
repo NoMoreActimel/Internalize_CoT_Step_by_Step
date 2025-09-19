fe3b4a9b00159f877c6be0024c5233ef826c9561
NoMoreActimel <evolution3000futurama@gmail.com>
2025-09-19 01:26:21 -0400

    local cluster changes

--- run_generate_shared_graph_cot.sh
+++ run_generate_shared_graph_cot.sh
@@ -3,19 +3,20 @@
 cd /home/ss19021/Internalize_CoT_Step_by_Step/ || exit
 
 SCRATCH_DIR="/scratch/ss19021/Internalize_CoT_Step_by_Step"
-OUTPUT_DIR="$SCRATCH_DIR/generated_datasets/shared_graph_cot_${NUM_NODES}n_${NUM_EDGES}e_${NUM_SAMPLES}s/"
+OUTPUT_DIR="$SCRATCH_DIR/generated_datasets/shared_graph_cot_${NUM_NODES}n_${NUM_EDGES}e_${NUM_SAMPLES}s_091925/"
 mkdir -p "$OUTPUT_DIR"
 
 CANDIDATE_SAMPLES_PATH="$OUTPUT_DIR/candidate_samples.json"
 DATASET_PATH="$OUTPUT_DIR/subprosqa_dataset.json"
+CONFIG_PATH="$OUTPUT_DIR/config.json"
 
-NUM_NODES=1000
-NUM_EDGES=3000
-NUM_SAMPLES=1000
+NUM_NODES=10000
+NUM_EDGES=50000
+NUM_SAMPLES=10000
 NUM_CONTEXT_EDGES=25
 REPRESENTATION="structured"
 DEPTH_MIN=3
-DEPTH_MAX=10
+DEPTH_MAX=6
 SEED=42
 SHOW_SAMPLES=5
 
@@ -29,5 +30,7 @@ python3 src/subprosqa/generate_shared_graph_cot.py \
     --seed "$SEED" \
     --candidate_samples_path "$CANDIDATE_SAMPLES_PATH" \
     --dataset_path "$DATASET_PATH" \
+    --config_path "$CONFIG_PATH" \
     --show_samples "$SHOW_SAMPLES" \
     > "$OUTPUT_DIR/log_generate_graph.txt" 2>&1
+
