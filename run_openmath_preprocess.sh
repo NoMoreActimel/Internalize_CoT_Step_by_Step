05bae18d8f85d5985da1428b177f1d9779ac5895
NoMoreActimel <evolution3000futurama@gmail.com>
2025-09-18 23:40:46 -0400

    included 2 cluster bash scripts due to git pull conflicts

--- run_openmath_preprocess.sh
+++ run_openmath_preprocess.sh
@@ -8,21 +8,26 @@ cd /home/ss19021/Internalize_CoT_Step_by_Step/
 
 # Scratch/dataset locations
 SCRATCH="/scratch/ss19021/Internalize_CoT_Step_by_Step"
-OUTDIR="${SCRATCH}/data/openmath"
+OUTDIR="${SCRATCH}/data/openmath-instruct-2-augGSM8k"
 
 mkdir -p "$OUTDIR"
 
-python prepare_openmath.py \
-  --path                    open-r1/OpenR1-Math-220k \
+# open-r1/OpenR1-Math-220k
+# nvidia/OpenMathInstruct-2
+
+python preprocess_huggingface.py \
+  --path                    nvidia/OpenMathInstruct-2 \
   --name                    default \
   --split                   train \
   --split_train_val_test \
+  --json_dataset \
   --val_size                0.1 \
   --test_size               0.1 \
   --split_random_state      42 \
-  --filter_max_str_length   1024 \
-  --filter_key              answer \
+  --tokenizer_model         "meta-llama/Llama-3.2-1B" \
+  --filter_column_name      "problem_source" \
+  --filter_column_values    "augmented_gsm8k" \
   --output_dir              "$OUTDIR" \
   > "$OUTDIR/prepare.log" 2>&1
 
-echo "Done. Files written to $OUTDIR/{train.txt,valid.txt,test.txt}"
+echo "Done. Files written to $OUTDIR/{train.json, valid.json, test.json}"
--- run_stepbystep.sh
+++ run_stepbystep.sh
@@ -2,18 +2,20 @@
 
 cd /home/ss19021/Internalize_CoT_Step_by_Step/ || exit
 
-D=7
+D=11
 SCRATCH_DIR="/scratch/ss19021/Internalize_CoT_Step_by_Step"
-DIR="$SCRATCH_DIR/train_models/${D}_by_${D}_mult/gpt2_pretrained/sbs_from_scratch/"
+DIR="$SCRATCH_DIR/train_models/${D}_by_${D}_mult/gpt2_pretrained/sbs_from_scratch_4_072225/"
 mkdir -p "$DIR"
 
 python src/train.py \
     --model gpt2 \
     --train_path "$SCRATCH_DIR/data/${D}_by_${D}_mult/train.txt" \
     --val_path "$SCRATCH_DIR/data/${D}_by_${D}_mult/valid.txt" \
+    --test_path "$SCRATCH_DIR/data/${D}_by_${D}_mult/test_bigbench.txt" \
     --epochs 32 \
     --lr 5e-5 \
-    --batch_size 64 \
+    --batch_size 32 \
+    --n_generative_eval_batches 1 \
     --accumulate 1 \
     --train_from_scratch \
     --removal_type step-by-step \
@@ -26,7 +28,8 @@ python src/train.py \
     --reset_optimizer \
     --save_model "$DIR" \
     --wandb_project cot-distillation \
-    --wandb_run_name sbs_${D}b${D}_from_scratch \
+    --wandb_run_name sbs_${D}b${D}_from_scratch_4 \
     --max_new_tokens 512 \
     --save_model "$DIR" \
+    --from_pretrained_checkpoint /scratch/ss19021/Internalize_CoT_Step_by_Step/train_models/11_by_11_mult/gpt2_pretrained/sbs_from_scratch_3_071725/checkpoint-epoch17.pth \
     > "$DIR/log_stepbystep.train" 2>&1
